a28f4c3dbc66bdfd4399ea7d3d1ad7fd0144e814
Balaji T <trbalaji95@gmail.com>
2024-04-19 19:02:00 +0530

    Homepage Bug Fixes

--- src/Module.js
+++ src/Module.js
@@ -7,9 +7,6 @@ const Module = () => {
   return (
     <Grid sx={{ backgroundColor: "#fcf2e5", fontFamily: "Lexend" }}>
       <HomePage />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
     </Grid>
   );
 };
